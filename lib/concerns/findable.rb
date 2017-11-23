e7a32fa059b1181a86023deae7b22c5a4772212b
Sherry El Garhy <sherryptk@gmail.com>
2017-11-23 17:58:09 +0000

    automatically backed up by learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -1,13 +1,7 @@
 module Findable
 
   def find_by_name(name)
-    self.class_variable.detect{|a| a.name == name}
+    self.all.detect{|a| a.name == name}
   end
-
-
-    def to_param
-      name.downcase.gsub(' ', '-')
-    end
-
 
 end
